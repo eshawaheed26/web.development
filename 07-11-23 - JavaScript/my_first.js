1edf582d5277df8811a16b34afc27232c426c2f8
Esha Waheed <eshawaheed26@gmail.com>
2023-11-07 11:59:06 +0500

    7-11-23

---
+++ 07-11-23 - JavaScript/my_first.js
@@ -0,0 +1,46 @@
+console.log("Esha,Wajeeha")
+console.log(typeof("hello"));
+console.log(typeof(123));
+console.log(typeof(false));
+console.log(typeof(123));
+
+ var a;
+console.log(typeof(a)); 
+
+
+var b;
+console.log(typeof(c)); 
+
+/* Sir Amish didn't told me NULL*/
+
+var currentTime = 10;
+console.log(currentTime>20 && currentTime<17);
+
+var currentTime = 10;
+console.log(currentTime>20 || currentTime<17);
+
+console.log(22%5);
+console.log(22/5);
+console.log(typeof(22/5));
+var petHungry = true;
+console.log(petHungry=!petHungry);
+
+console.log(5 == 5)
+console.log(5 == "5")
+
+console.log(5 === 5)
+console.log("5" === "5")
+
+console.log(5 == 4)
+
+console.log(12 + "months")
+var overtime=1;
+overtime +=7;
+console.log(overtime);
+
+var longString="";
+longString +="Once ";
+longString +="upon ";
+longString +="a ";
+longString +="time";
+console.log(longString);
