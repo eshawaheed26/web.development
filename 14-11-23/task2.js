f43c717ad2b229aebfe755e72faf8f1ea6313864
Esha Waheed <eshawaheed26@gmail.com>
2023-11-14 10:10:40 +0500

    Nested

--- 14-11-23/task2.js
+++ 14-11-23/task2.js
@@ -1,25 +1,25 @@
 var day = "Sunday";
 switch(day){
     case 'Monday':
-    console.log('Do something');
+    console.log('Do something good');
     break;
     case 'Tuesday':
-    console.log('Do something');
+    console.log('Do something bad');
     break;
     case 'Wednesday':
-    console.log('Do something');
+    console.log('Do something worst');
     break;
     case 'Thursday':
-    console.log('Do something');
+    console.log('Do something dumb');
     break;
     case 'Friday':
-    console.log('Do something');
+    console.log('Do something stupid');
     break;
     case 'Saturday':
-    console.log('Do something');
+    console.log('Do something creepy');
     break;
     case 'Sunday':
-    console.log('Do something');
+    console.log('Do something fun');
     break;
     default:
     console.log("There is no such day")
