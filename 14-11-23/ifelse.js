b1007150cee411c97c0c4e5c2f026bdd4917ee4a
Esha Waheed <eshawaheed26@gmail.com>
2023-11-14 08:51:05 +0500

    if/else

---
+++ 14-11-23/ifelse.js
@@ -0,0 +1,8 @@
+var result = 40;
+if (result > 50){
+    console.log("Congratulations you passed.");
+}
+else{
+    console.log("Unfortunately you did not pass");
+}
+
---
+++ 14-11-23/ifelse2.js
@@ -0,0 +1,12 @@
+var food = "Bad";
+if (food == "hot"){
+    console.log( 'Too hot');
+}
+
+else if (food == "cold"){
+    console.log( 'Too cold');
+}
+
+else{
+    console.log('Just right')
+}
