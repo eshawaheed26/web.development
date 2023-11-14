5fe4317e615a90001cd5ed804c41d9cda89398b6
Esha Waheed <eshawaheed26@gmail.com>
2023-11-14 09:01:22 +0500

    Switch

--- 14-11-23/ifelse.js
+++ 14-11-23/ifelse.js
@@ -6,3 +6,33 @@ else{
     console.log("Unfortunately you did not pass");
 }
 
+var food = "Bad";
+if (food == "hot"){
+    console.log( 'Too hot');
+}
+
+else if (food == "cold"){
+    console.log( 'Too cold');
+}
+
+else{
+    console.log('Just right')
+}
+
+
+var food = "first";
+if (food == "first"){
+    console.log( 'Gold');
+}
+
+else if (food == "second"){
+    console.log( 'Silver');
+}
+
+else if (food == "third"){
+    console.log( 'Bronze');
+}
+
+else{
+    console.log('No medal');
+}
