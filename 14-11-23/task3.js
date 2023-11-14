e1b5f6b1e84edb451c1d03811d3bdab4102b8022
Esha Waheed <eshawaheed26@gmail.com>
2023-11-14 10:25:13 +0500

    Task 3 Done

---
+++ 14-11-23/task3.js
@@ -0,0 +1,16 @@
+for ( var i = 1; i <= 10; i++){
+    if (i == 1){
+        console.log("Gold Medal");
+    }
+    if (i == 2){
+        console.log("Silver Medal");
+    }
+
+    if (i == 3){
+        console.log("Bronze Medal");
+    }
+
+    else if ( i >= 4 && i <= 10){
+        console.log(i);
+    }
+}
