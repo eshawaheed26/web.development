efae60970644e68fae7a5db693c47b57e797c24c
Esha Waheed <eshawaheed26@gmail.com>
2023-11-14 10:45:44 +0500

    Function

---
+++ 14-11-23/function.js
@@ -0,0 +1,9 @@
+// First use keyword of function=function, Name of Funtion, () - Input to Function
+// For calling the function, you write the function name
+
+function addTwoNums(){
+    var a = 10;
+    var b = 20;
+    var c = a + b;
+    console.log(c);
+} addTwoNums();
