f919cc0f44f4b1fd80bdc4718dd53aeabf469219
Esha Waheed <eshawaheed26@gmail.com>
2023-11-21 08:43:45 +0500

    Use of Math Random Function

---
+++ 21-11-23/mathrandom.js
@@ -0,0 +1,7 @@
+Math.random();
+var decimal = Math.random() * 10;
+var rounded = Math.trunc(decimal);
+console.log(rounded);
+//console.log(decimal * 10);
+
+// Similarily ceil, floor, and round function can be applied
