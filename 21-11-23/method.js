0e4a17995deb890a259db21b07e66e0e5703fc60
Esha Waheed <eshawaheed26@gmail.com>
2023-11-21 09:29:53 +0500

    Methods

---
+++ 21-11-23/method.js
@@ -0,0 +1,14 @@
+// When a function is defined in a object, it becomes a method
+var car = {};
+car.mileage = 98765;
+car.color = "red";
+console.log(car);
+car.turnTheKey = function(){
+    console.log("The engine is running")
+}
+car.lightsOn = function(){
+    console.log("The lights are on")
+}
+console.log(car);
+car.turnTheKey();
+car.lightsOn();
