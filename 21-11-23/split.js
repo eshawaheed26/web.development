e5fe30b5dec050ada6f85f58b104aa4a72539738
Esha Waheed <eshawaheed26@gmail.com>
2023-11-21 09:12:08 +0500

    Task 1 Done

--- 21-11-23/split.js
+++ 21-11-23/split.js
@@ -1,3 +1,7 @@
 var colour = "White-Black Pink-Purple";
 var find = colour.split("-");
 console.log(find);
+
+var greet = "Bye";
+var result= greet.toUpperCase("Bye");
+console.log(result);
---
+++ 21-11-23/task1.js
@@ -0,0 +1,9 @@
+var clothes = [];
+clothes.push("Tops");
+clothes.push("Jeans");
+clothes.push("Skirts");
+clothes.push("Trousers");
+clothes.push("Jackets");
+clothes.pop();
+clothes.push("Shirts");
+console.log(clothes[2]);
